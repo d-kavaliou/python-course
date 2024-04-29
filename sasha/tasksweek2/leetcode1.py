e2f015d37852459958704ad239fe9151ad93ad26
alexandra <alexandra@MacBook-Pro-alexandra.local>
2024-04-29 15:52:48 +0300

    add task 1

---
+++ sasha/tasksweek2/leetcode1.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = s.lower()
+        "converting all letters into lowercase letters"
+        s1 = list(filter(lambda x: x.isalnum(),s))
+        "removing all non-alphanumeric letters"
+        s2 = list(reversed(s1))
+        "checking the string is polindrom"
+        result = lambda s1,s2: True if s1 == s2 else False
+        return result(s1,s2)
+
+    
+solution = Solution()
+
+
+print(solution.isPalindrome("1A man, a plan, a canal: Panama1"))
+print(solution.isPalindrome("race a car"))
+print(solution.isPalindrome("11"))
