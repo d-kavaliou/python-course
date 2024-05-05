6af26439eefae76acb5f901086c1ac833529578d
alexandra <alexandra@MacBook-Pro-alexandra.local>
2024-05-05 12:33:18 +0300

    solution for task2 and attempt to create a website

---
+++ sasha/tasksweek2/task2.py
@@ -0,0 +1,38 @@
+# Try to guess Time Complexity of the following functions
+
+# Function 1
+def get_first_element(arr):
+    return arr[0]  # Accessing the first element is constant time O(1)
+
+
+# Example usage:
+array = [5, 4, 3, 2, 1]
+print(get_first_element(array))
+
+
+# Function 2
+def sum_elements(arr):
+    total = 0 
+    for element in arr:
+        total += element  # Sum each element, one at a time /0(N)
+    return total
+
+
+# Example usage:
+array = [5, 4, 3, 2, 1]
+print(sum_elements(array))
+
+
+# Function 3
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]  # Swap if elements are in wrong order
+    return arr #/O(n^2)
+
+
+# Example usage:
+array = [5, 3, 8, 6, 2]
+print(bubble_sort(array))
